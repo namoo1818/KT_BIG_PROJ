9b24b8c7e99964e3b83a5ff13df38d184d4d4f2a
YeonYeonni <79257220+YeonYeonni@users.noreply.github.com>
2022-12-29 16:50:22 +0900

    age_model

--- AI/age/age_predict.py
+++ AI/age/age_predict.py
@@ -1,65 +1,51 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[1]:
 
 
 import numpy as np
-import pandas as pd
-
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D,GlobalAveragePooling2D,Dropout, Flatten, Dense
-from keras import regularizers
-from keras.optimizers import SGD
-from keras.preprocessing.image import ImageDataGenerator
-from sklearn.metrics import roc_auc_score
-from sklearn.preprocessing import LabelEncoder # one-hot encoding for age
-from keras.utils.np_utils import to_categorical
-import keras
 import tensorflow.compat.v2 as tf
-
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
 from PIL import Image
-from sklearn.model_selection import train_test_split
-import cv2
-import os
-from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 
 
-# In[7]:
+# In[2]:
 
 
 model = tf.keras.models.load_model('age_model.h5')
 
 
-# In[12]:
+# In[3]:
 
 
-X, Y = [], []
-width = 180
-height = 180
+X = []
+width = 100
+height = 200
 
 image = Image.open('jy.jpg')
 image = image.convert('RGB')
 image = image.resize((width, height))
 X.append(np.array(image))
-Y.append(3)
 
 
-# In[13]:
+# In[4]:
 
 
 X = np.array(X)
 X = X.astype('float32')
 X /= 255.0
-Y = np.array(Y)
 
 
-# In[14]:
+# In[5]:
 
 
 result = model.predict(X)
+
+
+# In[6]:
+
+
+result
 
 
 # In[ ]:
